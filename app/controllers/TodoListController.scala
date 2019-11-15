d5c82793b1c3b08a8e05e9a05ca8a5b9845baf86
Nanami Mikiya <unison.0412@gmail.com>
2019-11-15 15:52:30 +0900

    add some functions

--- app/controllers/TodoListController.scala
+++ app/controllers/TodoListController.scala
@@ -1,13 +1,14 @@
 package controllers
 
 import javax.inject.{Inject, Singleton}
-import play.api.mvc.{AbstractController, ControllerComponents}
+import models.{Task, Tasks}
+import play.api.mvc.{AbstractController, ControllerComponents, Result}
 
 /**
   * TodoListコントローラ
   */
 @Singleton
-class TodoListController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class TodoListController @Inject()(tasks: Tasks)(cc: ControllerComponents) extends AbstractController(cc) {
 
   /**
     * インデックスページを表示
@@ -17,16 +18,52 @@ class TodoListController @Inject()(cc: ControllerComponents) extends AbstractCon
     Ok(views.html.index("Welcome to Play application!"))
   }
 
-  def tasks = TODO
+  def list = Action { request =>
+    val entries = tasks.list
+    Ok(views.html.list(entries))
+  }
 
-  def entry(id: Int) = TODO
+  def entry(id: Int) = Action {
+    tasks.findByID(id) match {
+      case Some(e) => Ok(views.html.entry(e))
+      case None    => NotFound(s"No entry for id=${id}")
+    }
+  }
 
-  def startRegistration = TODO
+  def comp(id: Int) = Action {
+    tasks.findByID(id) match {
+      case Some(e) =>
+        var comped = e
+        if (e.isDone == false) {
+          comped = Task(e.id, e.title, e.description, true, e.createdAt)
+        } else {
+          comped = Task(e.id, e.title, e.description, false, e.createdAt)
+        }
+        tasks.save(comped)
+      case None => NotFound(s"No entry for id=${id}")
+    }
+    Redirect(routes.TodoListController.list).withNewSession
+  }
 
-  def registerDate = TODO
+  def edit(id: Int) = TODO
 
-  def registerTask = TODO
+  def update(id: Int) = TODO
 
-  def confirm = TODO
+  def delete(id: Int) = TODO
+
+  def register = Action { request =>
+    Ok(views.html.register(request)).withNewSession
+  }
+
+  def confirm = Action { request =>
+    (for {
+      param       <- request.body.asFormUrlEncoded
+      title       <- param.get("title").flatMap(_.headOption)
+      description <- param.get("description").flatMap(_.headOption)
+    } yield {
+      tasks.save(Task(title, description, false))
+      Redirect("/todolist/tasks").withNewSession
+    }).getOrElse[Result](Redirect("/todolist/tasks/create"))
+  }
 
 }
--- app/models/Tasks.scala
+++ app/models/Tasks.scala
@@ -1,8 +1,8 @@
 package models
 
 import javax.inject.{Inject, Singleton}
-import scala.concurrent.ExecutionContext
 
+import scala.concurrent.ExecutionContext
 import play.api.db.slick.{DatabaseConfigProvider => DBConfigProvider}
 
 /**
@@ -17,11 +17,28 @@ class Tasks @Inject()(dbcp: DBConfigProvider)(implicit ec: ExecutionContext) ext
   val table = "task"
 
   /**
-   * DB上に保存されている全てのタスクを取得する
-   * @return
-   */
+    * DB上に保存されている全てのタスクを取得する
+    * @return
+    */
   def list: Seq[Task] = Await.result(
     db.run(sql"SELECT id, title, description, is_done, created_at FROM #$table".as[Task])
   )
+
+  def findByID(id: Int): Option[Task] = Await.result(
+    db.run(sql"SELECT id, title, description, is_done, created_at FROM #$table WHERE id=#$id".as[Task].headOption)
+  )
+
+  def save(task: Task): Int = task match {
+    case Task(0, title, description, isDone, _) =>
+      Await.result(
+        db.run(sqlu"INSERT INTO #$table (title, description, is_done) VALUES ('#$title', '#$description', '#$isDone')")
+      )
+    case Task(id, title, description, isDone, _) =>
+      Await.result(
+        db.run(
+          sqlu"UPDATE #$table SET title='#$title', description='#$description', is_done='#$isDone' WHERE id = #$id"
+        )
+      )
+  }
 
 }
